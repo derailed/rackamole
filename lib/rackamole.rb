5fdcfcdb23496a0f5bbb8a6eb9afe5a8d6592111
Fernand Galiana <fernand.galiana@gmail.com>
2010-07-16 08:48:07 -0600

    Rev 0.3.8

--- lib/rackamole.rb
+++ lib/rackamole.rb
@@ -1,7 +1,7 @@
 module Rackamole
 
   # :stopdoc:
-  VERSION = '0.3.8'
+  VERSION = '0.3.9'
   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
   # :startdoc:
